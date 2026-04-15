2b1329f985f3c04c4d782eb16d5192966ac020f2
cjahhh <mcjgarcia1@tip.edu.ph>
2026-04-15 17:55:24 +0800

    Add start.sh to set up environment and run services

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export MYSQLHOST=$(echo $MYSQLHOST)
+export MYSQLDATABASE=$(echo $MYSQLDATABASE)
+export MYSQLUSER=$(echo $MYSQLUSER)
+export MYSQLPASSWORD=$(echo $MYSQLPASSWORD)
+export MYSQLPORT=$(echo $MYSQLPORT)
+
+/usr/local/sbin/php-fpm -D
+sed -i "s/PORT_PLACEHOLDER/$PORT/g" /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
