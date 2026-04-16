743529bce0fe25161a195d3fd44e5b94b118f916
cjahhh <mcjgarcia1@tip.edu.ph>
2026-04-16 17:15:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export MYSQLDATABASE=$(echo $MYSQLDATABASE)
 export MYSQLUSER=$(echo $MYSQLUSER)
 export MYSQLPASSWORD=$(echo $MYSQLPASSWORD)
 export MYSQLPORT=$(echo $MYSQLPORT)
-
 /usr/local/sbin/php-fpm -D
+sleep 1
 sed -i "s/PORT_PLACEHOLDER/$PORT/g" /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
