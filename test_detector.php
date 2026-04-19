0cc921a44be449faff5332e3411a632ddc525af1
cjahhh <mcjgarcia1@tip.edu.ph>
2026-04-19 23:54:29 +0800

    Update test file

--- test_detector.php
+++ test_detector.php
@@ -1,7 +1,7 @@
 <?php
-// Find PHP-FPM config
-echo shell_exec('find / -name "www.conf" 2>/dev/null');
-echo "\n---\n";
-echo shell_exec('php-fpm8* -i 2>/dev/null | grep "Loaded Configuration"');
-echo "\n---\n";
-echo shell_exec('ls /etc/php*/');
+$url = getenv('PARKY_TF_SERVICE_URL')
+    ?: ($_ENV['PARKY_TF_SERVICE_URL'] ?? '')
+    ?: ($_SERVER['PARKY_TF_SERVICE_URL'] ?? '');
+var_dump($url);
+echo "Length: " . strlen($url) . "\n";
+echo "URL raw: [" . $url . "]\n";
