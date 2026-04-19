d0f2dad9859fc8ef5b2939ed4268efde54d5e431
cjahhh <mcjgarcia1@tip.edu.ph>
2026-04-19 23:42:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,14 @@ export MYSQLPASSWORD=$(echo $MYSQLPASSWORD)
 export MYSQLPORT=$(echo $MYSQLPORT)
 export PARKY_TF_SERVICE_URL=$(echo $PARKY_TF_SERVICE_URL)
 
+# Pass env vars to PHP-FPM
+echo "env[PARKY_TF_SERVICE_URL] = $(echo $PARKY_TF_SERVICE_URL)" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "env[MYSQLHOST] = $(echo $MYSQLHOST)" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "env[MYSQLDATABASE] = $(echo $MYSQLDATABASE)" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "env[MYSQLUSER] = $(echo $MYSQLUSER)" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "env[MYSQLPASSWORD] = $(echo $MYSQLPASSWORD)" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "env[MYSQLPORT] = $(echo $MYSQLPORT)" >> /etc/php/8.2/fpm/pool.d/www.conf
+
 /usr/local/sbin/php-fpm -D
 sed -i "s/PORT_PLACEHOLDER/${PORT}/g" /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
