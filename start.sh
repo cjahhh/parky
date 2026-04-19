2602ab53a5205c653bade03d5c0551da812206c7
cjahhh <mcjgarcia1@tip.edu.ph>
2026-04-19 23:46:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,12 +7,12 @@ export MYSQLPORT=$(echo $MYSQLPORT)
 export PARKY_TF_SERVICE_URL=$(echo $PARKY_TF_SERVICE_URL)
 
 # Pass env vars to PHP-FPM
-echo "env[PARKY_TF_SERVICE_URL] = $(echo $PARKY_TF_SERVICE_URL)" >> /etc/php/8.2/fpm/pool.d/www.conf
-echo "env[MYSQLHOST] = $(echo $MYSQLHOST)" >> /etc/php/8.2/fpm/pool.d/www.conf
-echo "env[MYSQLDATABASE] = $(echo $MYSQLDATABASE)" >> /etc/php/8.2/fpm/pool.d/www.conf
-echo "env[MYSQLUSER] = $(echo $MYSQLUSER)" >> /etc/php/8.2/fpm/pool.d/www.conf
-echo "env[MYSQLPASSWORD] = $(echo $MYSQLPASSWORD)" >> /etc/php/8.2/fpm/pool.d/www.conf
-echo "env[MYSQLPORT] = $(echo $MYSQLPORT)" >> /etc/php/8.2/fpm/pool.d/www.conf
+echo "env[PARKY_TF_SERVICE_URL] = $(echo $PARKY_TF_SERVICE_URL)" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[MYSQLHOST] = $(echo $MYSQLHOST)" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[MYSQLDATABASE] = $(echo $MYSQLDATABASE)" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[MYSQLUSER] = $(echo $MYSQLUSER)" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[MYSQLPASSWORD] = $(echo $MYSQLPASSWORD)" >> /usr/local/etc/php-fpm.d/www.conf
+echo "env[MYSQLPORT] = $(echo $MYSQLPORT)" >> /usr/local/etc/php-fpm.d/www.conf
 
 /usr/local/sbin/php-fpm -D
 sed -i "s/PORT_PLACEHOLDER/${PORT}/g" /etc/nginx/sites-enabled/default
