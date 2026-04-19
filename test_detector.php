fdf0198caae17f7d5005bfd174f3607f7d53dd62
cjahhh <mcjgarcia1@tip.edu.ph>
2026-04-19 23:34:32 +0800

    Update test_detector.php

--- test_detector.php
+++ test_detector.php
@@ -1,10 +1,5 @@
 <?php
 $url = getenv('PARKY_TF_SERVICE_URL');
-echo "URL: " . $url . "\n";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-$res = curl_exec($ch);
-$err = curl_error($ch);
-echo "Result: " . $res . "\n";
-echo "Error: " . $err . "\n";
+var_dump($url);
+echo "Length: " . strlen($url) . "\n";
+echo "URL raw: [" . $url . "]\n";
